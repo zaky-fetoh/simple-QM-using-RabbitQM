c86e9b9b2b723a0f394a4b4e09fa4ef25f377be1
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-20 20:30:24 +0200

    consumer route is added

--- consumer/index.js
+++ consumer/index.js
@@ -5,33 +5,53 @@ const morgan = require("morgan")
 ConsPORT = process.env.PORT || 3001
 RABBITMQ_HOST = process.env.RABBITMQ_HOST || "amqp://localhost:5672"
 
-QforTopic = {}; 
+//setting connectio to the RAbbitMQ
+const mqConnc = await amqp.connect(RABBITMQ_HOST);
+const chunnel = await mqConnc.createChannel();
+QforTopic = {};
 
-(async()=>{
+(async () => {
     express().use(morgan())
-    .use(express.urlencoded()).use(express.json())
-    .get("/listen-to/:topic",async(req, res,next)=>{
-        const topic = req.params.topic;
-        if(QforTopic[topic]) res.status(500).json({
-            ok:false, error: "Server is already listening",
-            Qid: QforTopic[topic],
+        .use(express.urlencoded()).use(express.json())
+        .get("/listen-to/:topic", async (req, res, next) => {
+            const topic = req.params.topic;
+            if (QforTopic[topic]) return res.status(500).json({
+                ok: false, error: "Server is already listening",
+                Qid: QforTopic[topic],
+            })
+            //Creating Q
+            const q = await chunnel.assertQueue("", {
+                durable: true,
+            });
+            QforTopic[topic] = q.queue;
+            //creating Exchange Point
+            const exch = chunnel.assertExchange("MASSAGES", "topic", {
+                durable: true,
+            });
+            //creating binding
+            chunnel.bindQueue(QforTopic[topic], exch, topic);
+            res.status(200).json({
+                QName: q.queue, 
+                ok:true, 
+            })
         })
-        //setting connectio to the RAbbitMQ
-        const mqConnc = await amqp.connect(RABBITMQ_HOST);
-        const chunnel = await mqConnc.createChannel();
-        //Creating Q
-        const q = await chunnel.assertQueue("",{
-            durable:true,
-        });
-        QforTopic[topic] = q.queue; 
-        //creating Exchange Point
-        const exch = chunnel.assertExchange("MASSAGES","topic",{
-            durable:true,
-        });
-        //creating binding
-        chunnel.bindQueue(QforTopic[topic],exch, topic)
+        .use("/consume/:topic", async (req, res, next) => {
+            const topic = req.params.topic ;
+            if(!(QforTopic[topic])) return res.status(500).json({
+                error: "this Topic is not rejestered for listens",
+                ok:false, 
+            });
+            const massages = []; 
+            let consumer = chunnel.consume(QforTopic[topic],m=>{
+                massages.push(JSON.parse(m.toString()))
+            },{noAck:true}); 
+            chunnel.cancel(consumer)
+            res.status(200).json({
+                ok:true, massages,
+            });
+        })
 
-    }).listen(ConsPORT,()=>{
-        console.log("Consumer Stared")
-    })
+        .listen(ConsPORT, () => {
+            console.log("Consumer Stared")
+        })
 })()
