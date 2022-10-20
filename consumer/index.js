5119052e7f98ffdf976a8a06760f674043ca8938
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-20 20:55:59 +0200

    refactor of main function

--- consumer/index.js
+++ consumer/index.js
@@ -2,23 +2,24 @@ const express = require("express");
 const amqp = require("amqplib");
 const morgan = require("morgan")
 
-ConsPORT = process.env.PORT || 3001
-RABBITMQ_HOST = process.env.RABBITMQ_HOST || "amqp://localhost:5672"
+ConsPORT = process.env.PORT || 3001;
+RABBITMQ_HOST = process.env.RABBITMQ_HOST || "amqp://localhost:5672";
 
-//setting connectio to the RAbbitMQ
-const mqConnc = await amqp.connect(RABBITMQ_HOST);
-const chunnel = await mqConnc.createChannel();
-QforTopic = {};
 
-async function clear(){
-    await mqConnc.close()
-    delete QforTopic
-}
-
-process.on("beforeExit", clear);
-process.on("exit", clear);
 
 (async () => {
+    //setting connectio to the RAbbitMQ
+    const mqConnc = await amqp.connect(RABBITMQ_HOST);
+    const chunnel = await mqConnc.createChannel();
+    QforTopic = {};
+
+    async function clear() {
+        await mqConnc.close()
+        delete QforTopic
+    }
+
+    process.on("beforeExit", clear);
+    process.on("exit", clear);
     express().use(morgan())
         .use(express.urlencoded()).use(express.json())
         .get("/listen-to/:topic", async (req, res, next) => {
@@ -39,23 +40,23 @@ process.on("exit", clear);
             //creating binding
             chunnel.bindQueue(QforTopic[topic], exch, topic);
             res.status(200).json({
-                QName: q.queue, 
-                ok:true, 
+                QName: q.queue,
+                ok: true,
             })
         })
         .use("/consume/:topic", async (req, res, next) => {
-            const topic = req.params.topic ;
-            if(!(QforTopic[topic])) return res.status(500).json({
+            const topic = req.params.topic;
+            if (!(QforTopic[topic])) return res.status(500).json({
                 error: "this Topic is not rejestered for listens",
-                ok:false, 
+                ok: false,
             });
-            const massages = []; 
-            let consumer = chunnel.consume(QforTopic[topic],m=>{
+            const massages = [];
+            let consumer = chunnel.consume(QforTopic[topic], m => {
                 massages.push(JSON.parse(m.toString()))
-            },{noAck:true}); 
+            }, { noAck: true });
             chunnel.cancel(consumer)
             res.status(200).json({
-                ok:true, massages,
+                ok: true, massages,
             });
         })
         .listen(ConsPORT, () => {
--- producer/index.js
+++ producer/index.js
@@ -2,36 +2,39 @@ const express = require("express");
 const morgan = require("morgan")
 const amqp = require("amqplib");
 
-ConsPORT = process.env.PORT || 3002
-RABBITMQ_HOST = process.env.RABBITMQ_HOST || "amqp://localhost:5672"
-
-//setting connectio to the RAbbitMQ
-const mqConnc = await amqp.connect(RABBITMQ_HOST);
-const chunnel = await mqConnc.createChannel();
+ConsPORT = process.env.PORT || 3002;
+RABBITMQ_HOST = process.env.RABBITMQ_HOST || "amqp://localhost:5672";
 
 
-async function clear(){
-    await mqConnc.close()
-}
-
-process.on("beforeExit", clear);
-process.on("exit", clear);
 
 
-(async()=>{
+
+
+(async () => {
+    //setting connectio to the RAbbitMQ
+    const mqConnc = await amqp.connect(RABBITMQ_HOST);
+    const chunnel = await mqConnc.createChannel();
+
+    async function clear() {
+        await mqConnc.close()
+    }
+
+    process.on("beforeExit", clear);
+    process.on("exit", clear);
+
     express().use(morgan()).use(express.urlencoded())
-    .use(express.json())
-    .post("/sendto/:topic",async(req,res, next)=>{
-        const data = Buffer.from(JSON.stringify(req.body)); 
-        const topic = req.params.topic;
-        const exch = chunnel.assertExchange("MESSAGES", "topic", {
-            durable: true,
-        });
-        chunnel.publish(exch,topic,data);
-        res.status(200).json({
-            message:"data sent",
-            ok:true, 
+        .use(express.json())
+        .post("/sendto/:topic", async (req, res, next) => {
+            const data = Buffer.from(JSON.stringify(req.body));
+            const topic = req.params.topic;
+            const exch = chunnel.assertExchange("MESSAGES", "topic", {
+                durable: true,
+            });
+            chunnel.publish(exch, topic, data);
+            res.status(200).json({
+                message: "data sent",
+                ok: true,
+            })
         })
-    })
 
 })(); 
